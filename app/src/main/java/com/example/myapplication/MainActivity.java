209d5dedecedcd56ad7f9f596b645a5e9cf62d03
yudha-pixel <sukmayudha48@gmail.com>
2021-11-10 04:44:21 +0700

    my first commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 // This is some demo app
+//  This app will demonstate CRUD
 
 public class MainActivity extends AppCompatActivity {
 
