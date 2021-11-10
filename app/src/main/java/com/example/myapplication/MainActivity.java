9230cf89753c5d52fcd63738569291d14def9654
yudha-pixel <sukmayudha48@gmail.com>
2021-11-10 13:40:05 +0700

    adding dashboard and add item file

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,32 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-// This is some demo app
-//  This app will demonstate CRUD
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddItem();
+            }
+        });
+    }
+
+    public void openAddItem() {
+        Intent intent = new Intent(this, addItem.class);
+        startActivity(intent);
     }
 }
